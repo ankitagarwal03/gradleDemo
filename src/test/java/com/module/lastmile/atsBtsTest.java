7bcff3cef1835fe8d9d94ae6b5240a7c58fa5263
Ankit Agarwal <ankit.agarwal@dealshare.in>
2023-10-30 14:10:15 +0400

    added the new proj

---
+++ src/test/java/com/module/lastmile/atsBtsTest.java
@@ -0,0 +1,10 @@
+package com.module.lastmile;
+
+import org.junit.jupiter.api.Test;
+
+public class atsBtsTest {
+    @Test
+    public void test1(){
+        System.out.println("inside 1");
+    }
+}
